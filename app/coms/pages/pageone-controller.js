19124aed17eeadde2dd97d1b017a64c8cc6853de
sergeyradkov <radkov@gmail.com>
2016-05-03 17:09:38 -0600

    rr

--- app/coms/pages/pageone-controller.js
+++ app/coms/pages/pageone-controller.js
@@ -3,8 +3,8 @@ angular.module('twopages')
    
    var poc = this;
     
-//	ImageService.getImage().then(function(imageData){
-//	poc.imageData = imageData;
-    poc.pic = "http://cdn.onlyinyourstate.com/wp-content/uploads/2016/01/15947325773_8c5adad068_k-700x312.jpg";
-    
+    ImageService.getImage().then(function(imageData){
+    poc.imageData = imageData;
+
+    }) 
 })
