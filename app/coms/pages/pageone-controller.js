3559b4a88a8e546515d754ec411aaced2efd0e1e
sergeyradkov <radkov@gmail.com>
2016-05-03 17:02:53 -0600

    qqq

--- app/coms/pages/pageone-controller.js
+++ app/coms/pages/pageone-controller.js
@@ -1,11 +1,10 @@
 angular.module('twopages')
     .controller('PageOneCon', function(ImageService){
-    var poc = this;
+   
+   var poc = this;
     
 //	ImageService.getImage().then(function(imageData){
 //	poc.imageData = imageData;
-poc.pic = "http://cdn.onlyinyourstate.com/wp-content/uploads/2016/01/15947325773_8c5adad068_k-700x312.jpg";
-        
-        
-//        });
+    poc.pic = "http://cdn.onlyinyourstate.com/wp-content/uploads/2016/01/15947325773_8c5adad068_k-700x312.jpg";
+    
 })
